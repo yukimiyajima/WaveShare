8941a28e18e5be25b07f319e919e9174644d743a
yuki miyajima <miyajima383838@gmail.com>
2020-06-15 18:59:52 +0900

    [update]topicとuserの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_093704) do
+ActiveRecord::Schema.define(version: 2020_06_15_094041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_06_15_093704) do
     t.float "lat", null: false
     t.float "lng", null: false
     t.text "image"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_06_15_093704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "topics", "users"
 end
