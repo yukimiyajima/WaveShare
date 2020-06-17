5f9109ed92822b0f28150f6bcce5a52a6f13515c
yuki miyajima <miyajima383838@gmail.com>
2020-06-18 03:57:46 +0900

    [design]ヘッダーと各アイコンをデザイン

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module WaveShare
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
