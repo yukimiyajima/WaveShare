30eb985a672eaa96360c2a3311057a4017e56e32
yuki miyajima <miyajima383838@gmail.com>
2020-06-18 00:19:34 +0900

    [add]index画面にgoodボタン表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(topic_id: params[:topic_id])
     @topic = @like.topic
-    @likes_count = Like.where(topic_id: @topic.id).count
   end
 
   def destroy
     @like = Like.find(params[:id]).destroy
     @topic = @like.topic
-    @likes_count = Like.where(topic_id: @topic.id).count
   end
 end
