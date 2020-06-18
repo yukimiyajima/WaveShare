edd4d750934c81ec1c7a8f4eb697b5c3c4688b2e
yuki miyajima <miyajima383838@gmail.com>
2020-06-18 17:54:18 +0900

    [add]commentのcontrullerとviewの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+      else
+        format.html { redirect_to topic_path(@topic) , notice: '投稿できませんでした' }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:topic_id, :content)
+  end
+end
