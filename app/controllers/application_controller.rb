512dbb2ceba086327092839251b9f7f54069a626
yuki miyajima <miyajima383838@gmail.com>
2020-06-20 06:23:05 +0900

    [add]ゲストユーザー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 end
