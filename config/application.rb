1ec812eb169f8a9c85e2ae51d1e99843bf199d72
yuki miyajima <miyajima383838@gmail.com>
2020-06-22 03:02:17 +0900

    [add]commentテスト追加

--- config/application.rb
+++ config/application.rb
@@ -4,11 +4,23 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module WaveShare
+module TestWaveshare
   class Application < Rails::Application
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+    
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:comment) { FactoryBot.create(:comment) }
+  let!(:topic) { comment.topic }
+  let!(:user) { comment.user }
+
+  it '内容がある場合は有効' do
+    expect(FactoryBot.create(:comment)).to be_valid
+  end
+
+  it '内容が無い場合は無効' do
+    comment = FactoryBot.build(:comment, content: nil)
+    comment.valid?
+    expect(comment.errors[:content]).to include(I18n.t('errors.messages.blank'))
+  end
+
+  it 'topic削除時に関連付いているcommentが削除される' do
+    expect { topic.destroy }.to change { Comment.count }.by(-1)
+  end
+  
+  it 'user削除時に関連付いているcommentが削除される' do
+    expect { user.destroy }.to change { Comment.count }.by(-1)
+  end
 end
