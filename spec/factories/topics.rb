4cb51c0e65c78cf1c96b8ce3612a84c6ce89a75a
yuki miyajima <miyajima383838@gmail.com>
2020-06-22 02:30:29 +0900

    [add]topicテスト追加

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :topic do
-    
+    title { 'タイトル' }
+    content { '内容' }
+    lat { '35.330816' }
+    lng { '139.429835' }
+    association :user
+
+    factory :goku_topic do
+      title { 'GokuTitle' }
+      content { 'GokuContent' }
+      association :user, factory: :goku
+    end
+
+    factory :vegeta_topic do
+      title { 'VegetaTitle' }
+      content { 'VegetaContent' }
+      association :user, factory: :vegeta
+    end
   end
 end
