21bcfd6986672a9f4659e1122ab7edab1dd48af9
yuki miyajima <miyajima383838@gmail.com>
2020-06-22 07:09:29 +0900

    [add]seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  name = Faker::JapaneseMedia::DragonBall.character
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    icon: open("./db/surfers/icon#{n}.jpg")
+  )
+end
+
+User.all.each do |user|
+  contents = ['いい波です', 'Good!', 'Fun Wave!', 'Small Wave', 'Surf!']
+  user.topics.create!(
+    title: contents[rand(0..4)],
+    content: contents[rand(0..4)],
+    lat: rand(35.31500..35.319000).round(6), 
+    lng: rand(139.360000..139.480000).round(6),
+    image: open("./db/waves/img0.jpg"),
+  )
+end
+
+20.times do |n|
+  Like.create!(
+    user_id: rand(1..10),
+    topic_id: rand(1..10),
+  )
+end
