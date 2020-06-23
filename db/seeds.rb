05ad3c9e85048f976d1da33b3131fc3bcbaa9c1c
yuki miyajima <miyajima383838@gmail.com>
2020-06-23 21:23:58 +0900

    [update]seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do |n|
+20.times do |n|
   name = Faker::JapaneseMedia::DragonBall.character
   email = Faker::Internet.email
   password = "password"
@@ -16,15 +16,15 @@ User.all.each do |user|
   user.topics.create!(
     title: contents[rand(0..4)],
     content: contents[rand(0..4)],
-    lat: rand(35.31500..35.319000).round(6), 
-    lng: rand(139.360000..139.480000).round(6),
+    lat: rand(35.31400..35.320000).round(9), 
+    lng: rand(139.350000..139.480000).round(9),
     image: open("./db/waves/img0.jpg"),
   )
 end
 
 20.times do |n|
   Like.create!(
-    user_id: rand(1..10),
-    topic_id: rand(1..10),
+    user_id: rand(1..20),
+    topic_id: rand(1..20),
   )
 end
